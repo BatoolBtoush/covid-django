ca4a2a7cfc4c8950f7efedeaa9b6578d7a732957
BatoolBtoush <batoolbtoush98@gmail.com>
2022-12-16 21:06:23 +0200

    still not done

--- covid_19_app/views.py
+++ covid_19_app/views.py
@@ -1,26 +1,24 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 import requests
-from django.views.generic import ListView
+# from django.views.generic import ListView
 
-# Create your views here.
 
 def home(request):
     first_response = requests.get('https://api.covid19api.com/world/total').json()
     # second_response = requests.get('https://api.covid19api.com/country/south-africa/status/confirmed?from=2020-09-06T00:00:00Z&to=2020-09-11T00:00:00Z').json()
-    second_response = requests.get('https://api.covid19api.com/countries').json()
+    second_response = requests.get('https://api.covid19api.com/summary').json()
+
+    if request.method == 'POST':
+        selected_country = request.POST['selected_country']
+        print('here', selected_country)
+
+
     my_list = []
-    for i in range(0, len(second_response)):
-        my_list.append(second_response[i]['Country'])
+    for i in range(0, len(second_response['Countries'])):
+        my_list.append(second_response['Countries'][i]['Country'])
     return render(request, 'home.html', {'first_response':first_response, 'my_list': my_list})
 
 
-# def search(request):
-#     response = requests.get('https://api.covid19api.com/countries').json()
-#     my_list = []
-#     for i in range(0, len(response)):
-#         my_list.append(response[i]['Country'])
-#     context = {'my_list': my_list}
-#     return render(request, 'home.html', context)
 
 
 def country(request):
