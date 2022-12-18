d3ab83ffe620c3f28e932bf1ae63801d084e75ed
BatoolBtoush <batoolbtoush98@gmail.com>
2022-12-18 14:39:54 +0200

    search feature finally worked

--- covid_19_app/views.py
+++ covid_19_app/views.py
@@ -10,6 +10,11 @@ def home(request):
 
     my_list = []
     req_js = []
+    dates_list = []
+    cases_list = []
+    bigger_list =zip(dates_list, cases_list)
+
+
     for i in range(0, len(second_response['Countries'])):
         my_list.append(second_response['Countries'][i]['Slug'])
 
@@ -23,14 +28,32 @@ def home(request):
             # print('request_json', request_json)
             # print('selected_country',selected_country)
             req_js.append(request_json)
-            print(req_js[0])
+            # print(len(req_js[0]))
+            for i in range(len(req_js[0])):
+                
+                # dicts = req_js[0][i]
+                # # print(dicts)
+                # # cases_and_date = req_js[0][i]
+                # case = dicts.get("Cases")
+                # date = dicts.get("Date")
+                cases = req_js[0][i]['Cases']
+                dates = req_js[0][i]['Date']
+                cases_list.append(cases)
+                dates_list.append(dates)
+                # new_dict = {case : date for i in req_js[0]}
+                # return new_dict
+                # print(cases_and_date)
         else:
             req_js.append(1)
 
     context = {
         'first_response':first_response, 
         'my_list': my_list,
-        'req_js': req_js
+        'req_js': req_js,
+        # 'new_dict': new_dict.items(),
+        # 'cases_list': cases_list,
+        # 'dates_list': dates_list
+        'bigger_list': bigger_list
 
     }
     
