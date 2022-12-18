c32b53c9369dc412e6236da2f02fe75d58691af3
BatoolBtoush <batoolbtoush98@gmail.com>
2022-12-18 12:13:57 +0200

    working on search

--- covid_19_app/views.py
+++ covid_19_app/views.py
@@ -9,17 +9,29 @@ def home(request):
     second_response = requests.get('https://api.covid19api.com/summary').json()
 
     my_list = []
+    req_js = []
     for i in range(0, len(second_response['Countries'])):
-        my_list.append(second_response['Countries'][i]['Country'])
+        my_list.append(second_response['Countries'][i]['Slug'])
 
-
-    if request.method=='POST':
+    if request.method =='POST':
         selected_country = request.POST['selected_country']
-        print('here', selected_country)
+        start_date= request.POST['start_date']
+        end_date= request.POST['end_date']
+        request_handler = requests.get(f"https://api.covid19api.com/country/{selected_country}/status/confirmed?from={start_date}T00:00:00Z&to={end_date}T00:00:00Z")
+        if request_handler.status_code == 200:
+            request_json = request_handler.json()
+            print(request_json)
+            req_js.append(request_json)
+        else:
+            req_js.append(1)
 
+    context = {
+        'first_response':first_response, 
+        'my_list': my_list,
+        'req_js': req_js
 
-    
-    return render(request, 'home.html', {'first_response':first_response, 'my_list': my_list})
+    }
+    return render(request, 'home.html', context)
 
 
 
@@ -28,20 +40,10 @@ def all_countries(request):
     first_response = requests.get('https://api.covid19api.com/summary').json()
     results =  len(first_response['Countries'])
     my_new_list = []
-    records_stats = {}
 
 
     for i in range(0, results):
         my_new_list.append(first_response['Countries'][i])
-
-    
-
-    if request.method=='POST':
-        # country = request.GET['country']
-        # response = requests.get(first_response['Countries']) 
-        print("hey")
-
-
-    
+  
     context = {'my_new_list': my_new_list}
     return render(request, 'allcountries.html', context)
